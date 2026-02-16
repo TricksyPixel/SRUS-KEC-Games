e8c65199629b5acfe387d53e5c36757a0324e44e
TricksyPixel <kieremj.ii@gmail.com>
2026-02-16 14:48:46 +0800

    Added to unit tests player_test.py

--- test/player_test.py
+++ test/player_test.py
@@ -9,17 +9,19 @@ class TestPlayer(unittest.TestCase):
     # Test Case 1: Test player created with username TricksyPixel, ID 123456 .
     def test_player_creation(self):
         # Return the object as a readable string
-        player_object_str = self.__str__()
+        player_object_str = self.player.__str__()
 
-        self.assertTrue(player_object_str, 'Player ID: 123456 \nPlayer Username: TricksyPixel')
+        print(f'Test case 1: Test player created with username TricksyPixel, ID 123456\n{player_object_str}\n')
+        self.assertTrue(player_object_str, "Created a player object successfully.")
 
     # Test Case 2: Test modifying uid and player_name
     def test_change_details(self):
         self.player._name = "SpongeBob"
         self.player._uid = "999999"
 
-        player_object_str = self.__str__()
+        player_object_str = self.player.__str__()
 
+        print(f'Test case 2: Modify player ID and Name\n{player_object_str}\n')
         self.assertTrue(player_object_str, 'Player ID: 999999 \nPlayer Username: SpongeBob')
 
 if __name__ == '__main__':
