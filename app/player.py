e8b5b64e081636ec669f7e8eff9489d5081ba798
TricksyPixel <kieremj.ii@gmail.com>
2026-02-16 12:22:13 +0800

    Created Player class and unit testing files.

--- app/player.py
+++ app/player.py
@@ -1,8 +1,8 @@
 class Player:
     def __init__(self, unique_id, player_name):
 
-
         self._uid = unique_id
         self._name = player_name
 
     def __str__(self):
+        return f'Player ID: {self._uid} \nPlayer Username: {self._name}'
---
+++ test/player_test.py
@@ -0,0 +1,6 @@
+import unittest
+from app.player import Player
+
+class TestPlayerID(unittest.TestCase):
+    def setUp(self):
+        self.Player =
