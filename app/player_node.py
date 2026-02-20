0b601bd3b3924edacf1c3e095cdd3aa3a9684cc9
TricksyPixel <KierEmJ.II@gmail.com>
2026-02-20 16:08:53 +0800

    - wip: PlayerNode class

--- app/player_node.py
+++ app/player_node.py
@@ -1,5 +1,17 @@
 class PlayerNode(object):
-    def __init__(self, player):
-        self.__player = player
-        self.__next_player_node = None
-        self.__previous_player_node = None
+    def __init__(self, __player=None, __next=None, __previous=None):
+        self.__player = __player
+        self.__next = __next
+        self.__previous = __previous
+
+    def get_player(self):
+        return self.__player
+    
+    def get_next_player(self):
+        return self.__next
+    
+    def get_previous_player(self):
+        return self.__previous_player
+    
+    def set_player(self, __player, __):
+        
