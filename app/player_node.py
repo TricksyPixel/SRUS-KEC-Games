fa13e7e22c9afb5fc0222c311fd25c6fda1ce25d
TricksyPixel <KierEmJ.II@gmail.com>
2026-02-23 12:43:03 +0800

    Modified PlayerNode class: - Added getters and setters to set up nodes. - Added a key for taking the uid from the player object.

--- app/player_node.py
+++ app/player_node.py
@@ -1,6 +1,15 @@
+from app.player_list import PlayerList
+
+key = None
+
 class PlayerNode(object):
-    def __init__(self, __player=None, __next=None, __previous=None):
-        self.__player = __player
+    def __init__(self, player, uid):
+        __next = None
+        __previous = None
+
+        key =  uid
+
+        self.__player = player
         self.__next = __next
         self.__previous = __previous
 
@@ -9,9 +18,21 @@ class PlayerNode(object):
     
     def get_next_player(self):
         return self.__next
-    
+
     def get_previous_player(self):
-        return self.__previous_player
+        return self.__previous
     
-    def set_player(self, __player, __):
-        
+    def set_player(self, __player):
+        self.__player = __player
+
+    def set_next_player(self, __next):
+        self.__next = __next
+
+    def __str__(self):
+        curr_player_msg = get_player(self)
+        next_player_node_msg = get_next_player(self)
+        prev_player_node_msg = get_previous_player(self)
+
+        return (f'Current player: {curr_player_msg}\n'
+                f'Next player node: {next_player_node_msg}\n'
+                f'Prev player node: {prev_player_node_msg}')
