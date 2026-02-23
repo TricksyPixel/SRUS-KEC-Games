3bac98ed7efc2aab749dc1453ab9b0a7837d43b4
TricksyPixel <KierEmJ.II@gmail.com>
2026-02-23 13:45:35 +0800

    wip: Working on PlayerList class - In the middle of implementing the methods to add nodes to the list.

--- app/player_list.py
+++ app/player_list.py
@@ -1,5 +1,29 @@
-class PlayerList(object):
-    def __init__(self):
-        self.__head = None
+from app.player_node import PlayerNode
+from app.player import Player
 
-    
+
+class PlayerList(object):
+    def __init__(self, player, uid):
+        self.__head = None
+        self.__tail = None
+        self.list_length = 0
+
+        key = uid
+
+
+    def is_empty(self):
+        return self.__head is None
+
+    def find(self):
+        current_player = self.__head
+
+        while current_player and
+
+    def insert_item(self, key):
+        new_player = PlayerNode()
+        if self.__head == None:
+
+
+    # def delete(key):
+
+    # def delete_head()
