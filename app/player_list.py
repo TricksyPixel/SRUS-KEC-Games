46a060b2609c9c4d7a289e1e5dfa8c0517be7b75
TricksyPixel <KierEmJ.II@gmail.com>
2026-03-06 11:47:31 +0800

    wip: working on player list and player node.

--- app/player_list.py
+++ app/player_list.py
@@ -1,5 +1,45 @@
+from app.player_node import PlayerNode
+from app.player import Player
+
 class PlayerList(object):
     def __init__(self):
-        self.__head = None
+        self.__first = None
+    
+    def is_empty(self):
+        return self.__first is None
 
+    def find(self, key):
+        current = self.__first
+        
+        while current and current.__player != key:
+            current = current.next
+        
+        return current
+    
+    def insert_first(self, key):
+        new_player_node = PlayerNode(key)
+        new_player_node.next = self.__first
+        self.__first = new_player_node
+
+    def delete(self, key):
+        current = self.__first
+        previous = self.__first
+        while current and current.__player != key:
+            previous = current
+            current = current.next
+
+        if current is not None:
+            if current == self.__first:
+                self.__first = self.__first.next
+            else:
+                previous.next = current.next
+
+        return current
+    
+    def delete_first(self):
+        temporary_node = self.__first
+        if self.__first is not None:
+            self.__first = self.__first.next
+
+        return temporary_node
     
--- app/player_node.py
+++ app/player_node.py
@@ -1,17 +1,14 @@
 class PlayerNode(object):
-    def __init__(self, __player=None, __next=None, __previous=None):
-        self.__player = __player
-        self.__next = __next
-        self.__previous = __previous
 
-    def get_player(self):
-        return self.__player
+    __player = None
+    __next = None
+    __previous = None
+
+    def __init__(self, player):
+        self.__player = player
+        self.__previous = None
+
+        self.__next = None
     
-    def get_next_player(self):
-        return self.__next
-    
-    def get_previous_player(self):
-        return self.__previous_player
-    
-    def set_player(self, __player, __):
-        
+    def __str__(self):
+        return f'Current: {self.__player} \nPrevious: {self.__previous} \nNext: {self.__next}'
